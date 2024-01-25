509255a9d012896a6f360c633c0b91a3d4717038
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-25 12:44:11 -0600

    fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,23 @@
-input = ("Enter a number : ")
+import time
 
+
+n = int(input("Enter the size : "))
+
+
+for input in range(n):
+   
+    if input%3==0:
+     time.sleep(1) #sleep for one second
+     print("fizz") 
+    if input%5==0:
+      time.sleep(1) #sleep for one second
+      print("buzz")
+    if input%5==0 and input%3==0:
+        time.sleep(1) #sleep for one second
+        print("fizzbuzz")
+    if input%5!=0 and input%3!=0:
+         time.sleep(1) #sleep for one second
+         print(input)
+     
+
+    
