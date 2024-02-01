4f45697fec36d3cfd727f292537c0a78ad0a2432
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-01 12:01:43 -0600

    valid anagram

---
+++ validAnagram2.py
@@ -0,0 +1,14 @@
+print("Welcome to the Valid Anagram checking program:")
+s=input("Enter a word \n")
+t=input("Enter a second word\n")
+
+def isAnagram(self, s:str, t:str) -> bool:
+    return sorted(s)==sorted(t)
+
+
+
+
+if (isAnagram(isAnagram, s, t)):
+    print("valid anagram")
+else:
+    print("invalid anagram")
