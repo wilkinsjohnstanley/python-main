0e2d6e12f971771be206c2af08b404bc78b6cac7
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-13 16:19:53 -0600

    didn't work

---
+++ serial.py
@@ -0,0 +1,10 @@
+import serial #Import Serial Library
+
+arduinoSerialData = serial.Serial('com5', 9600)
+
+while (1==1):
+    if(arduinoSerialData.inWaiting()>0):
+        myData = arduinoSerialData.readline().decode('ascii')
+        print (myData) 
+
+
