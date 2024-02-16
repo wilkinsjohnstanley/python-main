526dcd5d5fd82fbcbe6d80bf99b1091dac1097a1
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-16 14:38:59 -0600

    size function

--- VisualPython/simpleanime.py
+++ VisualPython/simpleanime.py
@@ -6,7 +6,7 @@ ceiling=box(pos=vector(0,5,0),color=color.white,length=10,width=10,height=.1)
 leftWall=box(pos=vector(0,0,5),color=color.white,length=10,width=.1,height=10)
 rightWall=box(pos=vector(0,0,-5),color=color.white,length=10,width=.1,height=10)
 backWall=box(pos=vector(-5,0,0),color=color.white,length=.1,width=10,height=10)
-#backWall=box(pos=vector(-5,0,0),color=color.white,size(.1, 10, 10))
+#backWall=box(pos=vector(-5,0,0),color=color.white,size=vector(.1, 10, 10))
 marble=sphere(radius=.75, color=color.blue)
 deltaX=.1
 xPos=0
