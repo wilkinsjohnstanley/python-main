d0d5aaafa87ec2c93d554e5c5c2120873590b803
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-16 13:39:19 -0600

    made a room

---
+++ VisualPython/room.py
@@ -0,0 +1,11 @@
+from vpython import *
+from time import *
+#vector(x,y,z)
+floor=box(pos=vector(0,-5,0),color=color.white,length=10,width=10,height=.1)
+ceiling=box(pos=vector(0,5,0),color=color.white,length=10,width=10,height=.1)
+leftWall=box(pos=vector(0,0,5),color=color.white,length=10,width=.1,height=10)
+rightWall=box(pos=vector(0,0,-5),color=color.white,length=10,width=.1,height=10)
+backWall=box(pos=vector(-5,0,0),color=color.white,length=.1,width=10,height=10)
+
+while True:
+    pass
---
+++ VisualPython/wall.py
@@ -0,0 +1,8 @@
+from vpython import *
+from time import *
+#vector(x,y,z)
+floor=box(pos=vector(0,-5,0),color=color.white,length=10,width=10,height=.1)
+ceiling=box(pos=vector(0,5,0),color=color.white,length=10,width=10,height=.1)
+
+while True:
+    pass
