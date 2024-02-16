5c1fdb7785cca19b8137091254b3bc9990cdb9c8
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-16 12:49:46 -0600

    yay experimenting with Visual Python

---
+++ VisualPython/ball.py
@@ -0,0 +1,30 @@
+from vpython import *
+ball=sphere(color=color.blue)
+
+
+sleep(1)
+ball.color=color.red
+sleep(1)
+ball.color=color.green
+sleep(1)
+ball.color=color.blue
+sleep(1)
+ball.color=color.red
+sleep(1)
+ball.color=color.green
+sleep(1)
+ball.color=color.blue
+sleep(1)
+ball.color=color.red
+sleep(1)
+ball.color=color.green
+sleep(1)
+ball.color=color.blue
+sleep(1)
+ball.color=color.red
+sleep(1)
+ball.color=color.green
+sleep(1)
+ball.color=color.blue
+while True:
+    pass
