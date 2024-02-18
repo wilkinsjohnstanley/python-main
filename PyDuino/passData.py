e6bcb333cda49f04ec43c82a9f73d2770d4e07ef
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-18 15:24:55 -0600

    data was passed! Touch down! Eagle has landed!

--- PyDuino/passData.py
+++ PyDuino/passData.py
@@ -1,2 +1,12 @@
 import time
 import serial
+#I created an object that can get data from Arduino
+arduinoData=serial.Serial('com5',9600)
+#give it a second to get up and running
+time.sleep(1)
+#create an infinite loop just like Arduino!
+while True:
+    while(arduinoData.inWaiting()==0):
+        pass
+    dataPacket=arduinoData.readline()
+    print(dataPacket)
