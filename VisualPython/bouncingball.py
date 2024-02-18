12668d11fba8180a408c48372cf369eb91085654
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-18 15:17:01 -0600

    getting Arduino to communicate with my Pycode@!

--- VisualPython/bouncingball.py
+++ VisualPython/bouncingball.py
@@ -12,16 +12,32 @@ leftWall=box(pos=vector(-roomWidth/2,0,0),size=vector(wallThickness,roomHeight,r
 rightWall=box(pos=vector(roomWidth/2,0,0),size=vector(wallThickness,roomHeight,roomDepth), color=color.white)
 marble=sphere(radius=mRadius,color=color.red)
 deltaX=.1
+deltaY=.1
+deltaZ=.1
+
 xPos=0
+yPos=1
+zPos=-1
+
 while True:
     rate(50)
     xPos=xPos+deltaX
+    yPos=yPos+deltaY
+    zPos=zPos+deltaZ
     #x right marble edge
     XRME=xPos+mRadius
-    #right wall edgeS
+    XLME=xPos-mRadius
+#y top marble edge/y bottom marble edge
+    Ytme=yPos+mRadius
+    Ybme=yPos-mRadius
+#z back marble edge/z front marble edge
+    Zbme=zPos-mRadius
+    Zfme=zPos+mRadius
+ #right wall edgeS
     RWE=roomWidth/2-wallThickness/2
     LWE=-roomWidth/2+wallThickness/2
-    XLME=xPos-mRadius
+
+    
     if (XRME>=RWE or XLME<LWE):
         deltaX=deltaX*(-1)
     marble.pos=vector(xPos,0,0)
