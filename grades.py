d73608e8159a95989a9ee8503baf3772f6a8cb68
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-22 14:54:51 -0600

    grade averages

--- grades.py
+++ grades.py
@@ -1,11 +1,11 @@
 grades=[]
+average=0
 size=int(input("How many grades do you have?"))
 #get grades
 for grade in range(size):
-    grade=int(input("Enter a grade: "))
+    grade=float(input("Enter a grade: "))
     grades.append(grade)
 for grade in grades:
-    print(grade)
+    average = round(average+grade/size, 2)
+print(f'The average of all your grades is {average}')
 
-
-#tell the grades back 
