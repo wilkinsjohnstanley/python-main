b686a920ca66bdac9bbc20cd9d6a90b8f97f78ec
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-22 19:42:50 -0600

    joystick time!

---
+++ PyDuino/joystick.py
@@ -0,0 +1,23 @@
+import time
+import serial
+#I created an object that can get data from Arduino
+arduinoData=serial.Serial('com5',9600)
+#give it a second to get up and running
+time.sleep(1)
+#create an infinite loop just like Arduino!
+while True:
+    while(arduinoData.inWaiting()==0):
+        pass
+    dataPacket=arduinoData.readline()
+
+    #b'Count: 1\r\n'
+
+    #remove the b
+    dataPacket=str(dataPacket,'utf-8')
+    #remove the giant spacing from Arduino's println 
+    dataPacket=dataPacket.strip('\r\n')
+
+    #unpack the data
+    splitPacket=dataPacket.split(",")
+    #now we have our data in the form of an array!!!
+    print(splitPacket)
