d133ccee53d1c3db7586a105d3980bfc904efe73
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-02-24 11:09:25 -0600

    okay

--- PyDuino/roomwithball.py
+++ PyDuino/roomwithball.py
@@ -1,12 +1,11 @@
 from vpython import *
 import time
 import serial
-#import joystickinfo
 
 arduinoData=serial.Serial('com5', 9600)
-roomX=12
+roomX=10
 roomY=10
-roomZ=16
+roomZ=15
 wallT=.5
 wallColor=vector(1,1,1)
 wallOpacity=.8
@@ -28,15 +27,15 @@ paddleZ=.2
 paddleOpacity=.8
 paddleColor=vector(0,.8,.9)
 paddle=box(size=vector(paddleX,paddleY,paddleZ), pos=vector(0,0,roomZ/2), color=paddleColor,opacity=paddleOpacity)
-
+#deltas are the velocity
 marbleX=0
-deltaX=.1
+deltaX=.06
 
 marbleY=0
-deltaY=.1
+deltaY=.06
 
 marbleZ=0
-deltaZ=.1
+deltaZ=.06
 
 while True:
     while arduinoData.inWaiting()==0:
@@ -56,20 +55,26 @@ while True:
     marbleY=marbleY+deltaY
     marbleZ=marbleZ+deltaZ
 
-
+#if it hits the left or right wall, it changes its x direction
     if marbleX+marbleR>(roomX/2-wallT/2) or marbleX-marbleR<(-roomX/2+wallT/2):
         deltaX=deltaX*(-1)
         marbleX=marbleX+deltaX
-
+#if it hits the top or bottom wall, it changes its y direction
     if marbleY+marbleR>(roomY/2-wallT/2) or marbleY-marbleR<(-roomY/2+wallT/2):
         deltaY=deltaY*(-1)
         marbleY=marbleY+deltaY
 
-#open side
-    if marbleZ+marbleR>(roomZ/2-wallT/2) or marbleZ-marbleR<(-roomZ/2+wallT/2):
+#if it hits the back wall, change direction
+    if marbleZ-marbleR<(-roomZ/2+wallT/2):
         deltaZ=deltaZ*(-1)
         marbleZ=marbleZ+deltaZ
-
+# but the front only if it intersects the paddle
+    if marbleZ+marbleR>=roomZ/2-wallT/2:
+        #if all 4 of these are true then we bounce
+        if marbleX>padX-paddleX/2 and marbleX<padX+paddleX/2 and marbleY>padY-paddleY/2 and marbleY<padY+paddleY/2:
+#how to bounce
+            deltaZ=deltaZ*(-1)
+            marbleZ=marbleZ+deltaZ
     marble.pos=vector(marbleX,marbleY,marbleZ)
     paddle.pos=vector(padX, padY, roomZ/2)
 
