c3fca8e9686bf77a4f3d9ecb6be875322cf9959b
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-03-05 17:37:00 -0600

    learned about classes

--- cointoss.py
+++ cointoss.py
@@ -7,12 +7,12 @@ class Coin:
 #toss the coin
     def toss(self):
         #if it's a 0, this is head, otherwise its tails
-        if random.randint(0,1):
-            self.sideup="Heads"
+        if random.randint(0,1)==0:
+            self.sideup='Heads'
         else:
-            self.sideup="Tails"
-def get_sideup(self):
-    return self.sideup
+            self.sideup='Tails'
+    def get_sideup(self):
+        return self.sideup
 
 #main function
 
@@ -24,3 +24,8 @@ def main():
     #toss the coin
     print('I will now toss the coin')
     my_coin.toss()
+
+    #display the side of the coin that is facing up
+    print('This side is up: ', my_coin.get_sideup())
+
+main()
