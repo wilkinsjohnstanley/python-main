b6407238d86a12be0767ad0fa880291b86461d81
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-03-06 14:44:04 -0600

    learned about OOP in py

--- passingParameters.py
+++ passingParameters.py
@@ -19,23 +19,28 @@ class BankAccount:
 
     def get_balance(self):
         return self.balance
+
+    def __str__(self):
+        return f'The current balance is: ${self.balance:0.2f}'
 def main():
     starting_balance=float(input('Enter starting balance: $'))
     #create object(instance)
     savings=BankAccount(starting_balance)
+    credit_account=BankAccount(starting_balance)
+    mortgage_account=BankAccount(starting_balance)
     #deposit a check
     deposit_check=float(input('How much is your deposit?: $'))
     print('Amount to be despoited: ', deposit_check)
     savings.deposit(deposit_check)
 
     #display balance
-    print(f'Your current balance is ${savings.get_balance():0.2f}')
-
+    #print(f'Your current balance is ${savings.get_balance():0.2f}')
+    print(savings)
     #make withdrawal
     cash=float(input('How much do you want to withdrawal?: $'))
     print('Amount being withdrawan: $', cash)
     savings.withdrawal(cash)
     #display balance
-    print(f'Your current balance is: ${savings.get_balance():0.2f}')
-
+    #print(f'Your current balance is: ${savings.get_balance():0.2f}')
+    print(savings)
 main()
