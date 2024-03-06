91b4910da83c921c86a713b7ca222a1221274862
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-03-06 15:27:57 -0600

    loop basics

---
+++ JavaScript/forloop.js
@@ -0,0 +1,16 @@
+for (i=0; i<=10; i++){
+    console.log("the value of i is ", i)
+}
+/*
+the value of i is  0
+the value of i is  1
+the value of i is  2
+the value of i is  3
+the value of i is  4
+the value of i is  5
+the value of i is  6
+the value of i is  7
+the value of i is  8
+the value of i is  9
+the value of i is  10
+*/
