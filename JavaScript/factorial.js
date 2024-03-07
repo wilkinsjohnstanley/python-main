a35c8d5babe65e389c07b0df23a2e60b8b57cbdd
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-03-07 17:21:25 -0600

    factorial

---
+++ JavaScript/factorial.js
@@ -0,0 +1,12 @@
+function factorial(n){
+    let result = 1
+    for(let i = 2; i<=n; i++){
+        result = result*i
+    }
+    return result
+}
+console.log(factorial(0))//1
+console.log(factorial(1))//1
+console.log(factorial(5))//120
+
+//because it's a for loop, it's O(n)
