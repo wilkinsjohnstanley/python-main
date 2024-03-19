d9b1af336298e37e10aecdcbebc9e28f5421575a
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-03-19 16:35:36 -0500

    finished!

--- inheritance.py
+++ inheritance.py
@@ -8,16 +8,21 @@ class Automobile:
     #setters
     def set_make(self, make):
         self.auto_make=make
+
     def set_year(self, year):
         self.auto_year=year
+
     def set_price(self, price):
         self.auto_price=price
+
     #getters/accessors return stuff
-    def get_make(self, make):
+    def get_make(self):
         return self.auto_make
-    def get_year(self, year):
+
+    def get_year(self):
         return self.auto_year
-    def get_price(self, price):
+
+    def get_price(self):
         return self.auto_price
     
 
@@ -32,6 +37,8 @@ class Car(Automobile):
         self.auto_doors=doors
     def get_doors(self):
         return self.auto_doors
+
+
 class Truck(Automobile):
 #subclass:Truck
     def __init__(self, make, year, price, drive_type):
@@ -46,8 +53,8 @@ class Truck(Automobile):
 class SUV(Automobile):
 #subclass: SUV
     def __init__(self, make, year, price, pass_cap):
-        Automobile.__init__(make,year,price, pass_cap)
-        self.auto_pass_cap
+        Automobile.__init__(self, make, year, price)
+        self.auto_pass_cap=pass_cap
     def set_pass_cap(self, pass_cap):
         self.auto_pass_cap=pass_cap
     def get_pass_cap(self):
@@ -61,9 +68,9 @@ def main():
     make=input('Enter SUV manufacturer:')
     year=int(input('Enter SUV year: '))
     price=float(input('Enter SUV price: $'))
-    pass_cap=int(input('Enter SUV passenger capacity: '))
+    cap=int(input('Enter SUV passenger capacity: '))
 
-    suv1=SUV(make, year, price, pass_cap)
+    suv1=SUV(make, year, price, cap)
     #print data
     print('\nCar Inventory\n')
     print('Make: ', car1.get_make())
@@ -75,14 +82,14 @@ def main():
    
     print('Make: ', truck1.get_make())
     print('Year: ', truck1.get_year())
-    print('Price:', truck1.get_doors())
+    print('Price:', truck1.get_price())
     print('Drivetrain:', truck1.get_drive_type())
 
     print()
 
     print('Make: ', truck1.get_make())
     print('Year: ', truck1.get_year())
-    print('Price:', truck1.get_doors())
+    print('Price:', truck1.get_price())
     print('Passenger Capacity:', suv1.get_pass_cap())
 main()
 
