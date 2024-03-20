c4f708a15d003a5522b37384b904037aec051056
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-03-20 17:24:18 -0500

    factory pattern

---
+++ final-project/factory.py
@@ -0,0 +1,20 @@
+#logistics management applicationo
+class Person:
+    def __init__(self, name, age, university, degree):
+        self.name=name.title()
+        self.age=age
+        self.university=university.title()
+        self.degree=degree.capitalize()
+    #classmethod
+    def from_csv_line(cls, line:str) ->"Person":
+        return cls(*line.strip().split(","))
+
+
+with open("people.csv", "r") as file:
+    lines = file.readlines()
+person_list=map(Person.from_csv_line, lines)
+
+for person in person_list:
+    print(
+        f"{person.name} is {person.age}, studying {person.degree} at {person.university}."
+    )
