33f44e59a39bcb745268940f83c3ed1f3359da1f
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-03-24 17:38:06 -0500

    more fun

---
+++ Arrays/reverse.py
@@ -0,0 +1,5 @@
+cars=["Accord", "BMW", "Cayenne", "Dump truck"]
+cars.sort()
+print(f"This is the cars array: {cars}")
+cars.sort(reverse=True)
+print(f"This is the same array in reverse: {cars}")
---
+++ Arrays/sort.py
@@ -0,0 +1,4 @@
+cars=["Toyota", "Honda", "Toyota", "Nissan", "Toyota"]
+print(f"This is the cars array unsorted: {cars}")
+cars.sort()
+print(f"This is the cars array after having been sorted: {cars}")
